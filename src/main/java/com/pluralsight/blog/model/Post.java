dfc337f5f77a2634ba66545ebc58e65aa502fea2
Diego Dias Ribeiro da Silva <ddrsdiego@hotmail.com>
2022-01-19 14:06:56 -0300

    add api version

--- src/main/java/com/pluralsight/blog/model/Post.java
+++ src/main/java/com/pluralsight/blog/model/Post.java
@@ -3,6 +3,8 @@ package com.pluralsight.blog.model;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -13,10 +15,18 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Version
+    private Long version;
+
+    @NotNull
+    @Size(min = 4, max = 100)
     private String title;
+
     @Column(length = 1000000)
     @Lob
     private String body;
+
     @Temporal(TemporalType.DATE)
     @CreationTimestamp
     private Date date;
@@ -24,7 +34,7 @@ public class Post {
     @ManyToOne(fetch = FetchType.EAGER)
     private Author author;
 
-    public Post() {
+    public Post()  {
         super();
     }
 
