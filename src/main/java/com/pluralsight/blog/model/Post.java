7fbf830406e1facc3b6e98d329c50974a7f78c76
Diego Dias Ribeiro da Silva <ddrsdiego@hotmail.com>
2022-01-19 11:24:29 -0300

    implements one to many relationship

--- src/main/java/com/pluralsight/blog/model/Post.java
+++ src/main/java/com/pluralsight/blog/model/Post.java
@@ -3,33 +3,32 @@ package com.pluralsight.blog.model;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-import java.lang.reflect.Method;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 @Entity
 public class Post {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
-    @Column(length=1000000)
+    @Column(length = 1000000)
     @Lob
     private String body;
     @Temporal(TemporalType.DATE)
     @CreationTimestamp
     private Date date;
 
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Author author;
+
     public Post() {
         super();
     }
 
-    public Post(String title, String body){//, Author author) {
+    public Post(String title, String body) {//, Author author) {
         this();
         this.title = title;
         this.body = body;
@@ -63,6 +62,14 @@ public class Post {
         return date;
     }
 
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
     public String getDateStr() {
         DateFormat outputFormatter = new SimpleDateFormat("MM/dd/yyyy");
         return outputFormatter.format(this.date);
@@ -76,8 +83,8 @@ public class Post {
     public boolean equals(Object obj) {
         if (!(obj instanceof Post))
             return false;
-        Post otherPost = (Post)obj;
+        Post otherPost = (Post) obj;
         return this.title.equals(otherPost.getTitle()) &&
-               this.body.equals(otherPost.getBody());
+                this.body.equals(otherPost.getBody());
     }
 }
